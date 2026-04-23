7e06218f811875abf3a569984330c0a21bf30b9b
Bruna Chapelin <bruna.polvina@gmail.com>
2026-04-23 01:34:51 -0300

    Initial Commit

--- src/pipeline/base.py
+++ src/pipeline/base.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from src.pipeline.context import PipelineContext
 
-
 class Filter(ABC):
     @abstractmethod
     def process(self, ctx: PipelineContext) -> PipelineContext:
