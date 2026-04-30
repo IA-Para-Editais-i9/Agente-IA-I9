202b4b17abf7a4c08a5f38fc640e8dbf0a42eb6e
Lacci <lannadosanjoscosta@gmail.com>
2026-04-29 21:21:11 -0300

    feat: add campos p coleções

--- src/pipeline/context.py
+++ src/pipeline/context.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass, field
 from typing import Optional
 
-
 @dataclass
 class PipelineContext:
     pdf_path: str = ""  # entrada inicial
     markdown_text: str = ""  # saída do f01_ingestion
-    chroma_collection_id: str = ""  # saída do f02_indexing
-    criterios_edital: Optional[dict] = None  # saída do f03_extraction
+
+    # mudei aqui -lanna
+    edital_collection_id: str = ""# saida f02 indexing 
+    empresa_collection_id: str = "" #docs i9
+
+    criterios_edital: Optional[dict] = None  # saída do f03_extraction:
     company_chunks: list[str] = field(default_factory=list)  # saída do f04_retrieval
     resultado_fit: Optional[dict] = None  # saída do f05_inference
